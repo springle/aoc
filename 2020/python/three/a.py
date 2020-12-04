f8bf88d4efeee746f1746f74ccce234b4b3de2f5
Sam Pringle <pringle@hey.com>
2020-12-03 20:41:06 -0500

    Clean up

--- 2020/python/three/a.py
+++ 2020/python/three/a.py
@@ -23,20 +23,10 @@ class Forest:
 
     @functools.lru_cache()
     def get_pattern(self, y: int) -> Tuple[int, List[int]]:
-        """
-        Returns a tuple of the SIZE of the line,
-        and the INDICES of trees.
-        """
         line = self.lines[y]
         return len(line), [m.start() for m in re.finditer(r"#", line)]
 
     def is_tree(self, x: int, y: int) -> bool:
-        """
-        Determine if the coordinate is a tree.
-
-        :param x: 0 = top-left
-        :param y: 0 = top-left
-        """
         size, indices = self.get_pattern(y)
         return x % size in indices
 
